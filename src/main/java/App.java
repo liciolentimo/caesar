ed39d9d39698e744ac7243bad7441e63fccb5b5d
liciolentimo <lentimo@gmail.com>
2019-01-27 14:55:51 +0300

    Added method encrypt to successfully print message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,41 @@
-import java.io.Console;
+//import java.io.Console;
+import javax.swing.JOptionPane;
+ 
 
 public class App{
     public static void main(String[] args){
-        Console myConsole = System.console();
-        System.out.println("Enter string to encrypt: ");
-        String encrypted = myConsole.readLine();
 
-        Caesar caesar = new Caesar();
+        String str = (JOptionPane.showInputDialog("Input Data to encypt:"));
+         
+
+        String key = (JOptionPane.showInputDialog("Input the key:"));
+
+        int keyLength=key.length();
+
+        String encrypted = encrypt(str, keyLength);
+
+        System.out.println("Encrypted message:" + encrypted);
     }
+        public static String encrypt(String str, int keyLength){
+        
+            String encrypted = "";
+            
+            for(int i=0; i < str.length(); i++) {
+                int chars = str.charAt(i);
+                if(Character.isUpperCase(chars)){
+                    chars = chars + (keyLength % 26);
+                    if (chars > 'Z')
+                    chars = chars - 26;
+                }
+                else if (Character.isLowerCase(chars)) {
+                    chars = chars + (keyLength % 26);
+                    if (chars > 'z')
+                    chars = chars - 26;
+                }
+                encrypted += (char) chars;
+                
+    
+            }
+            return encrypted;
+        }
 }
--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -5,4 +5,46 @@ public class Caesar{
         }
         return true;
     }
+    public boolean shiftValue(int shift){
+       if(shift>26){
+           return false;
+       }
+    //    else if(shift<0){
+    //        return false;
+    //    }
+       else{
+           return true;
+       }
+    }
+    
+
+    // public static String encrypt(String str, int keyLength){
+        
+    //     String encrypted = "";
+    //      str = "";
+    //      String key = "";
+         
+
+
+    //      keyLength=key.length();
+    //      encrypted = encrypt(str, keyLength);
+        
+    //     for(int i=0; i < str.length(); i++) {
+    //         int chars = str.charAt(i);
+    //         if(Character.isUpperCase(chars)){
+    //             chars = chars + (keyLength % 26);
+    //             if (chars > 'Z')
+    //             chars = chars - 26;
+    //         }
+    //         else if (Character.isLowerCase(chars)) {
+    //             chars = chars + (keyLength % 26);
+    //             if (chars > 'z')
+    //             chars = chars - 26;
+    //         }
+    //         encrypted += (char) chars;
+            
+
+    //     }
+    //     return encrypted;
+    // }
 }
